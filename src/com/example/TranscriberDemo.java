c8e41dd7065c82917b7cd9f5ea09603b0232a5e9
mrinmaysantra <mrinmaysantra@192.168.0.102>
2017-08-06 10:31:15 +0530

    renaming the WAV files

--- src/com/example/TranscriberDemo.java
+++ src/com/example/TranscriberDemo.java
@@ -14,18 +14,17 @@ public class TranscriberDemo {
 	public static void main(String[] args) throws Exception {
 
 		Configuration configuration = new Configuration();
-
 		configuration.setAcousticModelPath("resource:/edu/cmu/sphinx/models/en-us/en-us");
 		configuration.setDictionaryPath("resource:/edu/cmu/sphinx/models/en-us/cmudict-en-us.dict");
 		configuration.setLanguageModelPath("resource:/edu/cmu/sphinx/models/en-us/en-us.lm.bin");
 
 		StreamSpeechRecognizer recognizer = new StreamSpeechRecognizer(configuration);
-		//
-		//Microphone micro = new Microphone(44100, 16, true, false);
-		//micro.startRecording();
+		
+		Microphone micro = new Microphone(44100, 16, true, false);
+		micro.startRecording();
 		//InputStream stream = micro.getStream();
-		//
-		InputStream stream = new FileInputStream(new File("output.wav"));
+		InputStream stream = new FileInputStream(new File("mine.wav"));
+		
 		recognizer.startRecognition(stream);
 		SpeechResult result;
 		while ((result = recognizer.getResult()) != null) {
